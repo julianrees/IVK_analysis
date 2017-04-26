c66c7aa946dd8cde76fa8256c974c61922d1c254
Julian Rees <julianrees@users.noreply.github.com>
2017-04-26 15:54:04 -0700

    added standard dev, plotting faceted dose responses with error bars for concentration and activity separately.

--- IVK_workup.R
+++ IVK_workup.R
@@ -28,35 +28,24 @@ for(i in seq(1:nrow(template))){
     act[i,j] <- act[i,j-1] / template$Serial.Factor[i]
   }
 }
-
-
 melt(t(conc))[,3]
-
 rm(mtemp)
-
 mtemp <- melt(template[,1:20], id = colnames(template)[1:idCols])
-
-
 mtemp <- mtemp[with(mtemp, order(ExpNum)), ]
-
-
 mtemp <- cbind(mtemp, melt(t(conc))[,3])
 mtemp <- cbind(mtemp, melt(t(act))[,3])
 
+# - - - STD
 
 
+mtemp <- cbind(mtemp, melt(t(template[,21:28]))[,3])
+drop(mtemp$variable)
 
 
+colnames(mtemp) <- c(colnames(mtemp)[1:12], 'n1', 'Viability', 'Concentration', 'Activity', 'STD')
 
-
-
-
-
-
-
-
-
-
+mtemp$Viability <- mtemp$Viability * 100
+mtemp$STD <- mtemp$STD * 100
 
 #---------- END OF DATA MANIPULATION !-!-! PLOTTING STARTS ----------------
 w = 0.65
@@ -64,3 +53,31 @@ fwid = 9
 fhei = 6
 theme_set(theme_bw())
 theme_update(plot.title = element_text(hjust = 0.5))
+
+
+ggplot(mtemp, aes(x = Concentration, y = Viability, by = ExpNum)) + 
+  geom_errorbar(aes(ymin=Viability-STD, ymax=Viability+STD), color="black", width=.1) + 
+  geom_point() + 
+  scale_x_log10() + 
+  facet_wrap(~ExpNum) + 
+  labs(x = "Concentration (nM)", y = "% Viability")
+  
+
+ggplot(mtemp, aes(x = Activity, y = Viability, by = ExpNum)) + 
+  geom_errorbar(aes(ymin=Viability-STD, ymax=Viability+STD), color="black", width=.1) + 
+  geom_point() + 
+  scale_x_log10() + 
+  facet_wrap(~ExpNum)
+  
+
+
+
+
+
+
+
+
+
+
+
+
