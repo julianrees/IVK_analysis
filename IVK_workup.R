b3138d7db418b222ff1a06696f6a23ccb951796b
Julian Rees <julianrees@users.noreply.github.com>
2017-04-27 09:24:20 -0700

    cleaned up code, added comments

--- IVK_workup.R
+++ IVK_workup.R
@@ -2,8 +2,9 @@ library(ggplot2)
 library(reshape2)
 library(xlsx)
 library(plyr)
-rm(template)
 
+
+#--- Data import and conversion to numeric ---
 template <- read.xlsx("template2.xlsx", header = TRUE, sheetIndex = 1)
 template <- template[-1,]
 
@@ -11,11 +12,15 @@ template$High.Concentration <- as.numeric(levels(template$High.Concentration)[te
 template$High.Activity <- as.numeric(levels(template$High.Activity)[template$High.Activity])
 
 
+#--- Set the number of ID columns ---
+idCols <- 12
+
+
+#--- Extrapolate concentration and activity data from initial values and serial dilution factor ---
 conc <- matrix(nrow = 8, ncol = 8)
 for(i in seq(1:nrow(template))){
   conc[i,1] = template$High.Concentration[i]
   for(j in seq(1:7)+1){
-    print(j)
     conc[i,j] <- conc[i,j-1] / template$Serial.Factor[i]
   }
 }
@@ -24,28 +29,28 @@ act <- matrix(nrow = 8, ncol = 8)
 for(i in seq(1:nrow(template))){
   act[i,1] = template$High.Activity[i]
   for(j in seq(1:7)+1){
-    print(j)
     act[i,j] <- act[i,j-1] / template$Serial.Factor[i]
   }
 }
-melt(t(conc))[,3]
-rm(mtemp)
+
+
+#--- Melt the data to pairs, add concentration and activity, cleanup ---
 mtemp <- melt(template[,1:20], id = colnames(template)[1:idCols])
 mtemp <- mtemp[with(mtemp, order(ExpNum)), ]
 mtemp <- cbind(mtemp, melt(t(conc))[,3])
 mtemp <- cbind(mtemp, melt(t(act))[,3])
+mtemp <- mtemp[, -(idCols+1)]
 
-# - - - STD
 
-
+#--- Get the STD data from the template import, rename cols ---
 mtemp <- cbind(mtemp, melt(t(template[,21:28]))[,3])
-drop(mtemp$variable)
+colnames(mtemp) <- c(colnames(mtemp)[1:12], 'Viability', 'Concentration', 'Activity', 'STD')
 
 
-colnames(mtemp) <- c(colnames(mtemp)[1:12], 'n1', 'Viability', 'Concentration', 'Activity', 'STD')
-
+#--- Convert data and errors to percentages ---
 mtemp$Viability <- mtemp$Viability * 100
 mtemp$STD <- mtemp$STD * 100
+
 
 #---------- END OF DATA MANIPULATION !-!-! PLOTTING STARTS ----------------
 w = 0.65
@@ -67,7 +72,8 @@ ggplot(mtemp, aes(x = Activity, y = Viability, by = ExpNum)) +
   geom_errorbar(aes(ymin=Viability-STD, ymax=Viability+STD), color="black", width=.1) + 
   geom_point() + 
   scale_x_log10() + 
-  facet_wrap(~ExpNum)
+  facet_wrap(~ExpNum) + 
+  labs(x = "Concentration (nM)", y = "% Viability")
   
 
 
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This respository contains **R code** for the import, analysis, and plotting of *in vitro* killing assays and related data.
 
 
-####Initialized by:<br>
+###Initialized by:<br>
 Julian Rees<br>
 LBNL Chemical Sciences Division<br>
 26 Apr 2017
